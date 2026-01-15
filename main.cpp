2f5e34dc3f97dc9ec1cbb90f58d1272703903858
firedestroyer53 <firedestroyer53@gmail.com>
2026-01-15 00:39:57 -0500

    more refactoring

--- main.cpp
+++ main.cpp
@@ -3,35 +3,42 @@
 #include <iostream>
 #include "Solver.h"
 
-#define input_port true
-#define output_port false
+#define type_input true
+#define type_output false
 
 int main() {
     auto s = std::make_unique<Solver>();
-
-    auto i1 = std::make_unique<InOut>(true);
-    auto i2 = std::make_unique<InOut>(true);
+    
+    // create 2 inputs, a nand gate, and an output
+    auto i1 = std::make_unique<InOut>(type_input);
+    auto i2 = std::make_unique<InOut>(type_input);
     auto nand = std::make_unique<NandGate>();
-    auto output = std::make_unique<InOut>(false);
+    auto o1 = std::make_unique<InOut>(type_output);
 
-    s->addWire(i1->getPort(output_port, 0), nand->getPort(input_port, 0));
-    s->addWire(i2->getPort(output_port, 0), nand->getPort(input_port, 1));
-    s->addWire(nand->getPort(output_port, 0), output->getPort(input_port, 0));
+    // add a wire from the first output port of i1 to the first input port of nand
+    s->addWire(i1->getPort(type_output, 0), nand->getPort(type_input, 0));
+    // add a wire from the first output port of i2 to the second input port of nand
+    s->addWire(i2->getPort(type_output, 0), nand->getPort(type_input, 1));
+    // add a wire from the first output port of nand to the first input port of o1
+    s->addWire(nand->getPort(type_output, 0), o1->getPort(type_input, 0));
 
+    // transfer the nodes to the solver (note, the ownership of the pointers is transferred to the solver)
     s->addNode(std::move(i1));
     s->addNode(std::move(i2));
     s->addNode(std::move(nand));
-    s->addNode(std::move(output));
-    
+    s->addNode(std::move(o1));
+
     std::vector<Node*> inputs = s->getInputs();
     std::vector<Node*> outputs = s->getOutputs();
 
-    inputs[0]->setState(input_port, 0, true);
-    inputs[1]->setState(input_port, 1, true);
+    // set the state of the inputs
+    inputs[0]->setState(type_input, 0, true);
+    inputs[1]->setState(type_input, 1, true);
 
     s->pubSolve();
 
-    std::cout << outputs[0]->getPort(output_port)->state << std::endl;
+    // print the state of the output
+    std::cout << outputs[0]->getPort(type_output)->state << std::endl;
 
     return 0;
 }
